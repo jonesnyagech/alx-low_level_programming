5fcfe1f14ee5d4ae53eacc73ccc7e106039ec540
jonesnyagech <jonesnyagechi16@gmail.com>
2023-09-20 04:14:19 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * rot13 - function that encodes to rot13
+ *
+ * @s: param s
+ *
+ * Return: s
+ */
+
+
+char *rot13(char *s)
+{
+	int i, j;
+
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == alpha[j])
+			{
+				s[i] = rot[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
