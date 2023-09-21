b5eaf31ff01228802e71cebdf413e98e43d95d24
jonesnyagech <jonesnyagechi16@gmail.com>
2023-09-21 12:43:06 -0700

    done

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,50 +1,50 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_buffer - prints buffer of a char string
- *
- * @b: buffer to print
- * @size: size of buffer
- *
+ * print_buffer - prints buffer
+ * @b: buffer
+ * @size: size
  * Return: void
  */
 
 void print_buffer(char *b, int size)
 {
-	int i, j;
+	int o, j, i;
+
+	o = 0;
 
 	if (size <= 0)
-		printf("\n");
-	else
 	{
-		for (i = 0; i < size; i += 10)
+		printf("\n");
+		return;
+	}
+	while (o < size)
+	{
+		j = size - o < 10 ? size - o : 10;
+		printf("%08x: ", o);
+		for (i = 0; i < 10; i++)
 		{
-			printf("%08x: ", i);
-			j = 0;
-			while (j < 10)
+			if (i < j)
+				printf("%02x", *(b + o + i));
+			else
+				printf("  ");
+			if (i % 2)
 			{
-				if (j % 2 == 0 && j != 0)
-					printf(" ");
-				if ((j + i) > size - 1)
-				{
-					printf("  ");
-				}
-				else
-					printf("%.2x", b[i + j]);
-				j++;
+				printf(" ");
 			}
-			printf(" ");
-			j = 0;
-			while (j < 10)
-			{
-				if ((j + i) > size - 1)
-					break;
-				if (b[j + i] <= 31 || b[j + 1] >= '~')
-					b[j + i] = '.';
-				putchar(b[j + i]);
-				j++;
-			}
-			printf("\n");
 		}
+		for (i = 0; i < j; i++)
+		{
+			int c = *(b + o + i);
+
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		o += 10;
 	}
 }
