1ab7fd8e3155564285cb957c541afbbd8f1f6a01
jonesnyagech <jonesnyagechi16@gmail.com>
2023-09-25 14:00:08 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -18,24 +18,4 @@ void print_chessboard(char (*a)[8])
 		}
 		_putchar('\n');
 	}
-}#include "main.h"
-/**
- *print_chessboard - prints the chessboard
- *@a:array input
- *
- *Return:void
- *
- */
-void print_chessboard(char (*a)[8])
-{
-	int i, j;
-
-	for (i = 0; i < 8; i++)
-	{
-		for (j = 0; j < 8; j++)
-		{
-			_putchar(*(*(i + a) + j));
-		}
-		_putchar('\n');
-	}
 }
