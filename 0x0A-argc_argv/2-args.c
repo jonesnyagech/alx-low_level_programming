7e7a13896f049ab47a4e23cd90b718d42119c43a
jonesnyagech <jonesnyagechi16@gmail.com>
2023-09-29 13:49:07 -0700

    done

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints all arguements passed on to it followed by newline
+ *
+ * @argc: arguement count
+ * @argv: arguement vector
+ *
+ * Return: always return 0(success)
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
+
