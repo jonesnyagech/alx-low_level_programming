183209dcda3f256d3f3e968151b3963385156885
jonesnyagech <jonesnyagechi16@gmail.com>
2023-10-05 11:00:39 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,101 +1,103 @@
-#include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
-
-#define ERR_MSG "Error"
+#include <stdlib.h>
+#include "main.h"
 
 /**
- * is_digit - checks if a string contains a non-digit char
- * @s: string to be evaluated
+ * word_len - finds the length of a word
+ * @str: string to test
  *
- * Return: 0 if a non-digit is found, 1 otherwise
- */
-int is_digit(char *s)
-{
-	int i = 0;
+ * Return: int
+*/
 
-	while (s[i])
+int word_len(char *str)
+{
+	int i = 0, len = 0;
+
+	while (*(str + i) && *(str + i) != ' ')
 	{
-		if (s[i] < '0' || s[i] > '9')
-			return (0);
+		len++;
 		i++;
 	}
-	return (1);
+	return (len);
 }
 
 /**
- * _strlen - returns the length of a string
- * @s: string to evaluate
+ * word_count - counts the number of words
  *
- * Return: the length of the string
- */
-int _strlen(char *s)
-{
-	int i = 0;
+ * @str:input
+ *
+ * Return:(no. of words)
+ *
+*/
 
-	while (s[i] != '\0')
+int word_count(char *str)
+{
+	int i = 0, len = 0, count = 0;
+
+	for (i = 0; *(str + i); i++)
 	{
-		i++;
+		len++;
 	}
-	return (i);
-}
-
-/**
- * errors - handles errors for main
- */
-void errors(void)
-{
-	printf("Error\n");
-	exit(98);
-}
-
-/**
- * main - multiplies two positive numbers
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: always 0 (Success)
- */
-int main(int argc, char *argv[])
-{
-	char *s1, *s2;
-	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
-
-	s1 = argv[1], s2 = argv[2];
-	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
-		errors();
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	len = len1 + len2 + 1;
-	result = malloc(sizeof(int) * len);
-	if (!result)
-		return (1);
-	for (i = 0; i <= len1 + len2; i++)
-		result[i] = 0;
-	for (len1 = len1 - 1; len1 >= 0; len1--)
+	for (i = 0; i < len; i++)
 	{
-		digit1 = s1[len1] - '0';
-		carry = 0;
-		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		if (*(str + i) != ' ')
 		{
-			digit2 = s2[len2] - '0';
-			carry += result[len1 + len2 + 1] + (digit1 * digit2);
-			result[len1 + len2 + 1] = carry % 10;
-			carry /= 10;
+			count++;
+			i += word_len(str + i);
 		}
-		if (carry > 0)
-			result[len1 + len2 + 1] += carry;
 	}
-	for (i = 0; i < len - 1; i++)
+	return (count);
+}
+
+/**
+ * strtow - splits a string into words
+ *
+ * @str: input
+ *
+ * Return: 0 - success
+ *
+ */
+
+char **strtow(char *str)
+{
+	int i, words, w, letters, l;
+	char **p;
+
+	if (str == NULL || str[0] == '\0')
 	{
-		if (result[i])
-			a = 1;
-		if (a)
-			_putchar(result[i] + '0');
+		return (NULL);
 	}
-	if (!a)
-		_putchar('0');
-	_putchar('\n');
-	free(result);
-	return (0);
+	words = word_count(str);
+	if (words == 0)
+	{
+		return (NULL);
+	}
+	p = malloc(sizeof(char *) * (words + 1));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < words; i++)
+	{
+		while (*(str + w) == ' ')
+		{
+			w++;
+		}
+		letters = word_len(str + w);
+		p[i] = malloc(sizeof(char) * (letters + 1));
+		if (p[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(p[i]);
+			free(p);
+			return (NULL);
+		}
+		for (l = 0; l < letters; l++)
+		{
+			p[i][l] = str[w++];
+		}
+		p[i][l] = '\0';
+	}
+	p[i] = NULL;
+	return (p);
 }
