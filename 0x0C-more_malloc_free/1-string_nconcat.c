c1e3d6ac7ddfe1c42b60c397d5041463fe081e2a
jonesnyagech <jonesnyagechi16@gmail.com>
2023-10-05 08:37:11 -0700

    done

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,35 +1,45 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
 
 /**
- * _strdup - returns a pointer to a copy of string
+ * *string_nconcat - concatenates n bytes of a string to another string
+ * @s1: string to append to
+ * @s2: string to concatenate from
+ * @n: number of bytes from s2 to concatenate to s1
  *
- * @str: string param
- *
- * Return: NULL(fail),pointer
-*/
-
-char *_strdup(char *str)
+ * Return: pointer to the resulting string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i = 0, len = 0;
 	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (str == NULL)
-	{
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (!s)
 		return (NULL);
-	}
-	for (i = 0; str[i] != '\0'; i++)
-		len++;
-	s = malloc(sizeof(char) * (len + 1));
-	if (s == NULL)
+
+	while (i < len1)
 	{
-		return (NULL);
+		s[i] = s1[i];
+		i++;
 	}
-	for (i = 0; str[i]; i++)
-	{
-		s[i] = str[i];
-	}
-	s[len] = '\0';
+
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+
+	s[i] = '\0';
+
 	return (s);
 }
